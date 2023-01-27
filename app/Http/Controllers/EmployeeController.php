13f6752aa70c2228120c8d14859c1758af010310
unknown <axxeman1994@gmail.com>
2023-01-26 21:57:25 -0500

    proyecto en laravel de una apiRest

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Employee;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    public function index()
+    {
+        $employee = Employee::all();
+        return response()->json($employee);
+    }
+
+
+   /* public function store(Request $request)
+    {
+        $employees = new Employee([
+            'name' => $request->input('name'),
+            'address' => $request->input('address'),
+            'mobile' => $request->input('mobile'),
+ 
+        ]);
+        $employees->save();
+        return response()->json('Employee created!');
+    }*/
+}
